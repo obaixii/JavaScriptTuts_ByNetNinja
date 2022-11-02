8d1b1010ce7b2f2ca56dc3f6d5173c77e88346ba
Obaid Awan <obaidawan567@gmail.com>
2022-11-02 20:13:30 +0500

    Class #9 Math Operators Short-Hand

---
+++ class9/index.js
@@ -0,0 +1,25 @@
+var myVar = 10
+console.log(myVar)
+
+// myVar = myVar - 5
+// the above expression can be done as followed below
+myVar -= 5
+console.log(myVar)
+myVar += 5
+console.log(myVar)
+myVar *= 5
+console.log(myVar)
+myVar /= 5
+console.log(myVar)
+
+// Increament
+myVar++
+console.log(myVar)
+++myVar
+console.log(myVar)
+
+// Decreament
+myVar--
+console.log(myVar)
+--myVar
+console.log(myVar)
