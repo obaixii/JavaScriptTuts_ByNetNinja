84c1ebc5c5d845867d4fa900bc24acf63e579792
Obaid Awan <obaidawan567@gmail.com>
2022-11-03 19:41:33 +0500

    Class #14 Comparison Operators

---
+++ classs14/index.js
@@ -0,0 +1,7 @@
+var x = 5
+
+console.log(x < 5) //x is not less than 5 (output - false)
+console.log(x <= 5) // x is equals to 5 but not less than 5 (output - true)
+console.log(x == 5) // x is equals to 5 (output - true)
+console.log(x === "5") // x is equals to 5 but it is not a string (output - false)
+console.log(x === 5) // x is equals to 5 and it is a number (output - true)
