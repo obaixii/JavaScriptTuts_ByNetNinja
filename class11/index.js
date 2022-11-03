134954d651ea06262b44faa16278de0cb253fc5d
Obaid Awan <obaidawan567@gmail.com>
2022-11-03 08:30:05 +0500

    Class #11 Booleans

---
+++ class11/index.js
@@ -0,0 +1,11 @@
+myVar =  true
+console.log(myVar)
+
+num = 5
+console.log(7 < num)
+
+name = "obaid"
+console.log(name == "obaid")
+// output will be false , because of strict comparison
+// name is assigned to small case, while matched with tilte case
+console.log(name === "Obaid") 
