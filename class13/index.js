8e1aeae74375c5b04705447372d19651a6772353
Obaid Awan <obaidawan567@gmail.com>
2022-11-03 09:03:43 +0500

    Class #13 If.. Else If.. Else Conditional Statements

---
+++ class13/index.js
@@ -0,0 +1,13 @@
+var myAge = 21
+
+if (myAge >= 40) {
+    console.log("You are old")
+}else if (myAge >= 30) {
+    console.log("You are adult")
+}else if (myAge >= 20) {
+    console.log("You are young")
+}else if (myAge >= 13) {
+    console.log("You are teenager")
+}else{
+    console.log("you are kid")
+}
