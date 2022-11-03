aba0a1a1ce1897d89029d4ff84470909667fc7f3
Obaid Awan <obaidawan567@gmail.com>
2022-11-03 08:40:47 +0500

    Class #12 If Else Statements JavaScript Conditional Statements

---
+++ class12/index.js
@@ -0,0 +1,13 @@
+myNum = 2
+userName = "Obaid"
+if(myNum > 3){
+    console.log("myNum is greater than 3")
+}else{
+    console.log("myNum is less than 3")
+}
+
+if(userName == ""){
+    console.log("Username Not defined")
+}else{
+    console.log(userName)
+}
