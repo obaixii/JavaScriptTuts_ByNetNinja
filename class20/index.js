958289f52499fdd3cc02289ca30c9de16526d8ad
Obaid Awan <obaidawan567@gmail.com>
2022-11-04 16:22:43 +0500

    Class #20 Funtions in JavaScript

---
+++ class20/index.js
@@ -0,0 +1,19 @@
+
+
+function getAverage(a,b,c,d,e,f){
+    var average = (a+b+c+d+e+f) /2
+    return console.log(average)
+}
+getAverage(4,4,9,2,3,7)
+
+
+var num1 = 5
+var num2 = 10
+
+function add(num1,num1){
+    let avg = num1+num2
+    return avg
+}
+
+var myRes = add(num1,num1)
+console.log(myRes)
