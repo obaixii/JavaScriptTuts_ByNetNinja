12428a0bd23008b7d72ad0a3b4095d684d0a3bc5
Obaid Awan <obaidawan567@gmail.com>
2022-11-05 08:54:11 +0500

    Class #22 Numbers in JavaScript Math object in JavaScript

---
+++ class22/index.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 5;
+
+console.log(a+b);
+console.log(typeof(a+b));
+
+console.log(Math.round(7.5))
+console.log(Math.floor(7.5))
+console.log(Math.ceil(7.5))
+console.log(Math.max(7,4,9,6))
+console.log(Math.PI)
