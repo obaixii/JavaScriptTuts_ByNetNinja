3dd49d6f28a37efa48ae8838aa3f8cc5fe7de283
Obaid Awan <obaidawan567@gmail.com>
2022-11-05 13:48:34 +0500

    Class #24 Strings in JavaScript Strings Methods and Search

---
+++ class24/index.js
@@ -0,0 +1,20 @@
+var myString1 = "This is a 'String' with double quotation mark";
+var myString2 = 'This is a "String" with single quotation mark';
+var myString3 = "My name is obaid";
+
+
+console.log(myString1)
+console.log(myString2)
+
+// converts a string to Lower Case
+console.log(myString3.toLowerCase())
+
+// finds an index inside a string - returns -1 if not found
+console.log(myString3.indexOf("obaid"))
+
+
+var string1 = "abc"
+var string2 = "abc"
+
+console.log(string1==string2)
+console.log(string1<string2)
