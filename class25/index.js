4700981b4c093932a547907ef151123378f4dba5
Obaid Awan <obaidawan567@gmail.com>
2022-11-06 13:53:03 +0500

    Class #25 Splice and Split (Strings Method)

---
+++ class25/index.js
@@ -0,0 +1,15 @@
+// JavaScript is character Based, starting point of character starts from 0
+
+// Splice Method
+var str = "hello, world"
+console.log(str)
+var str2 = str.slice(3)
+var str3 = str.slice(3,9)
+console.log(str2)
+console.log(str3)
+
+// Split Method
+var tags = "meat,chicken,beef,fish,salami"
+console.log(tags)
+var tagsArray = tags.split(",")
+console.log(tagsArray)
