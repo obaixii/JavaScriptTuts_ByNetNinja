79f4b73801d1cacaaa52bcc01723e1d4905022ab
Obaid Awan <obaidawan567@gmail.com>
2022-11-06 14:27:59 +0500

    Class #26 Arrays in JavaScript Arrays Methods and Properties

---
+++ class26/index.js
@@ -0,0 +1,17 @@
+var myArray = ["Obaid","Shaun","Powell","Harry"]
+console.log(myArray)
+console.log(myArray[0])
+console.log(myArray[1])
+console.log(myArray[2])
+console.log(myArray[3])
+
+myArray[2] = "Fireship"
+console.log(myArray)
+
+//The length property sets or returns the number of elements in an array.
+console.log(myArray.length)
+
+console.log(myArray.sort())
+
+console.log(myArray.reverse())
+
